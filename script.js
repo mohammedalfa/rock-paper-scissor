0930e512018a4032e95e229d10b88e02132bfac3
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-04-19 22:37:35 -0400

    complete game functions

--- script.js
+++ script.js
@@ -22,3 +22,44 @@ function getPlayerChoice() {
         return getPlayerChoice();
     }
 }
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "Rock beats scissors, you lose!";
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        return "Paper beats rock, you lose!";
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        return "Scissors beats paper, you lose!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock beats scissors, you win!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Paper beats rock, you win!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Scissors beats paper, you win!";
+    } else if (playerSelection === computerSelection) {
+        return ("Tie!")
+    }
+}
+
+function game() {
+    let computerWins = 0;
+    let playerWins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let game = playRound(getComputerChoice(), getPlayerChoice());
+        console.log(game);
+        if (game.slice(-5) === "lose!") {
+            computerWins++;
+        } else if (game.slice(-4) === "win!") {
+            playerWins++;
+        }
+    }
+
+    if (computerWins > playerWins) {
+        console.log("You lost...");
+    } else if (playerWins > computerWins) {
+        console.log("You won!");
+    } else {
+        console.log("You tied!");
+    }
+}
